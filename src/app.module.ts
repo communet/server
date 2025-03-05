bc31d6b4e771fd3d3950550cc1cef23da3d8fb2c
Crinax <agreedeth@gmail.com>
2025-03-05 16:48:07 +0300

    feat(app): add typeorm, fix docker-compose.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
-import { ConfigSchema } from '@/config/schema';
+import { Config, ConfigSchema } from '@/config/schema';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ZodValidationPipe } from 'nestjs-zod';
 
 @Module({
@@ -11,6 +12,18 @@ import { ZodValidationPipe } from 'nestjs-zod';
     ConfigModule.forRoot({
       isGlobal: true,
       validate: (config) => ConfigSchema.parse(config),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<Config>) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+      }),
     }),
   ],
   controllers: [AppController],
