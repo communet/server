51bdf13a3a47865373e02b7928bc047cfe610774
Mick <infast1k@mail.ru>
2025-03-10 23:25:57 +0300

    feat(repositories): register credentials repository

--- src/application/api/auth/auth.module.ts
+++ src/application/api/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { AuthController } from '@/application/api/auth/auth.controller';
+import {
+  CredentialsRepositoryProvider,
+  IRegisterCommandHandler,
+  NestJsRegisterCommandHandlerProvider,
+} from '@/infra/nest-providers/auth-handlers.providers';
 import { Module } from '@nestjs/common';
 
 @Module({
   controllers: [AuthController],
-  providers: [],
+  providers: [
+    CredentialsRepositoryProvider,
+    NestJsRegisterCommandHandlerProvider,
+  ],
+  exports: [IRegisterCommandHandler],
 })
 export class AuthModule {}
