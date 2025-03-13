991c4d4a25a7fbfedacc483dc94dd6da2380d998
Mick <infast1k@mail.ru>
2025-03-14 00:00:09 +0300

    feat(infra): implement verify password function

--- src/infra/utils/password.utils.ts
+++ src/infra/utils/password.utils.ts
@@ -1,6 +1,13 @@
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
 
 export async function hashPassword(password: string): Promise<string> {
   const hashedPassword = await hash(password);
   return hashedPassword;
+}
+
+export async function verifyPassword(
+  password: string,
+  confirm: string,
+): Promise<boolean> {
+  return await verify(confirm, password);
 }
