5c9f6403137e81752af844a9d9a560f267f016d0
mi.nasonov <mi.nasonov@mail.ru>
2025-07-18 23:02:24 +0300

    feat(entities): provide message entity to chat entity (#985525)

--- src/core/entities/chat/chat.entity.ts
+++ src/core/entities/chat/chat.entity.ts
@@ -1,17 +1,23 @@
 import { StringRule } from '../../rules';
 import { Entity } from '../../abstracts';
 import { CHAT_NAME_OPTIONS } from './constants';
+import { MessageEntity } from '../message';
 
 export class ChatEntity extends Entity {
   protected readonly _name: StringRule;
-  // TODO: добавить список сообщений
+  protected readonly _messages: MessageEntity[];
 
-  constructor(id: string, name: string) {
+  constructor(id: string, name: string, messages?: MessageEntity[]) {
     super(id);
     this._name = new StringRule(name, CHAT_NAME_OPTIONS);
+    this._messages = messages ?? [];
   }
 
   public get name(): string {
     return this._name.value;
+  }
+
+  public get messages(): MessageEntity[] {
+    return this._messages;
   }
 }
