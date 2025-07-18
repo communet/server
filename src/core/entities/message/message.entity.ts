89a969adcad6d1b4bc66783c2eeafec26b7490fb
mi.nasonov <mi.nasonov@mail.ru>
2025-07-18 23:01:33 +0300

    feat(entities): add message entity (#985525)

---
+++ src/core/entities/message/message.entity.ts
@@ -0,0 +1,23 @@
+import { StringRule } from '../../rules';
+import { Entity } from '../../abstracts';
+import { MESSAGE_BODY_OPTIONS } from './constants';
+
+export class MessageEntity extends Entity {
+  // TODO: добавить идентификатор создателя
+  protected readonly _body: StringRule;
+  protected readonly _createdAt: Date;
+
+  constructor(id: string, body: string, createdAt?: Date) {
+    super(id);
+    this._body = new StringRule(body, MESSAGE_BODY_OPTIONS);
+    this._createdAt = createdAt ?? new Date();
+  }
+
+  public get body(): string {
+    return this._body.value;
+  }
+
+  public get createdAt(): Date {
+    return this._createdAt;
+  }
+}
