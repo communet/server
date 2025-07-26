5d8dc3307c0d0a243b22edce32fc3e85fd25d871
Crinax <agreedeth@gmail.com>
2025-07-26 16:03:24 +0300

    fix(core/chat-ports): fix by review

--- src/core/ports/chat/primary/commands/create-chat.command.ts
+++ src/core/ports/chat/primary/commands/create-chat.command.ts
@@ -1,3 +1,6 @@
 export class CreateChatCommand {
-  constructor(public readonly name: string) {}
+  constructor(
+    public readonly name: string,
+    public readonly channelId: string,
+  ) {}
 }
