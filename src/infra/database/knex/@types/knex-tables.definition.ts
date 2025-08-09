167ef661e23dcaee2fcee5d984276b6ad17de3dc
Crinax <agreedeth@gmail.com>
2025-08-10 02:27:18 +0300

    feat(knex): add user table and types for it

--- src/infra/database/knex/@types/knex-tables.definition.ts
+++ src/infra/database/knex/@types/knex-tables.definition.ts
@@ -1,6 +1,10 @@
+import { User } from './tables';
+
 // NOTE: https://knexjs.org/guide/#typescript
 declare module 'knex/types/tables' {
   // NOTE: Будет расширяться в дальнейшем
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface Tables {}
+
+  interface Tables {
+    users: User;
+  }
 }
---
+++ src/infra/database/knex/migrations/20250809232220_add_user_table.ts
@@ -0,0 +1,12 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary();
+    table.string('username').notNullable().unique();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
