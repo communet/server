c90531d71ccb275220103f2d189039e254dabcb0
Crinax <agreedeth@gmail.com>
2025-09-19 15:50:37 +0300

    fix(router): fix plugin definition for route with prefix

--- src/infra/fastify/router/user/user.router.ts
+++ src/infra/fastify/router/user/user.router.ts
@@ -1,11 +1,8 @@
-import fp from 'fastify-plugin';
+import { FastifyInstance } from 'fastify';
 import { createUserController } from '../../controllers/user/user.controller';
 
-export const UserRouter = fp(
-  (fastify) => {
-    const userController = createUserController();
+export const UserRouter = (fastify: FastifyInstance): void => {
+  const userController = createUserController();
 
-    fastify.get('/user/:id', userController.getUserById.bind(userController));
-  },
-  { name: 'user-router' },
-);
+  fastify.get('/user/:id', userController.getUserById.bind(userController));
+};
